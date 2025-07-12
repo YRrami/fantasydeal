cb3c56a0f105c64d4f49b6ebba59c85f511a806c
YRrami <johnjohn444465@gmail.com>
2025-07-12 04:30:38 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./App.css";
-import logo from './assets/logo.png';
+import logo from './1.png';
 
 const NAV_LINKS = [
   { label: "Home", href: "#home" },
